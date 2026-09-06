1d343b234b35e65e73108507a1fc9243f88387aa
qiyizoro <116663317+qiyizoro@users.noreply.github.com>
2026-09-06 21:28:32 +0800

    修复 GitHub Pages 空白页面

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './AppV2';
-import './styles.css';
-import './editor.css';
-import './secret.css';
-import './twin.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
