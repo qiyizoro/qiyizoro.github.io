4b5b6f0f280586785b2993bcd7127585fe3be6fb
qiyizoro <qiyideguge@gmail.com>
2026-09-06 17:05:45 +0800

    发布有椰之境初版

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './AppV2';
+import './styles.css';
+import './editor.css';
+import './secret.css';
+import './twin.css';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
